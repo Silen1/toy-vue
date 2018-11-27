33fc80aa78f7b2faef7be0467b2b72dec5313896
周欢 <zhouhuan@souche.com>
2018-11-27 18:12:33 +0800

    完成数组变异方法的拦截

--- src/core/index.js
+++ src/core/index.js
@@ -9,7 +9,8 @@ let data = {
       now: 'rockets',
       before: 'clippers'
     }
-  }
+  },
+  stars: ['kobe', 'jordan', 'wade']
 }
 
 // 收集依赖的篮子
@@ -50,7 +51,6 @@ function myWatch(target, depFn) {
   if (/\./.test(target)) {
     let helpData = data
     target.split('.').forEach((path) => {
-      console.log(1, path)
       helpData = helpData[path]
     })
     return
@@ -62,19 +62,43 @@ function render() {
   document.body.innerText = `The last season's MVP is ${data.name}, he's from ${data.team}, he has a teammate ${data.teammate.name}, ${data.teammate.name} was played for ${data.teammate.team.before}`
 }
 
-// myWatch(render, render)
-myWatch('name', () => {
-  console.log('name被修改了', data.name)
-})
-myWatch('name', () => {
-  console.log('name的第二个依赖', data.name)
+// 要代理的变异方法集合
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+]
+const arrayProto = Array.prototype
+const arrayMethods = Object.create(arrayProto)
+methodsToPatch.forEach((method) => {
+  arrayMethods[method] = function (...args) {
+    console.log('this', this)
+    arrayProto[method].apply(this, args)
+    console.log(`我捕获了数组的${method}方法`)
+  }
 })
 
-myWatch('teammate.team.before', () => {
-  console.log('以前的球队被修改了了了')
-})
+data.stars.__proto__ = arrayMethods
+
+// myWatch(render, render)
+// myWatch('name', () => {
+//   console.log('name被修改了', data.name)
+// })
+// myWatch('name', () => {
+//   console.log('name的第二个依赖', data.name)
+// })
+
+// myWatch('teammate.team.before', () => {
+//   console.log('以前的球队被修改了了了')
+// })
 
 // data.name = 'eleven'
 // data.team = 'xian'
 // data.teammate.name = 'jason'
 // data.teammate.team.before = 'lvdian'
+// data.stars.push('nash')
+// console.log(data.stars)
