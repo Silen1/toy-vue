f998bca606ef64dc771cd4baec95024fe35efeb4
周欢 <zhouhuan@souche.com>
2018-11-28 15:31:07 +0800

    封装Observer类

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,4 @@
-import { isPlainObj } from '../shared/util'
+import {observe, myWatch} from './observer'
 
 let data = {
   name: 'James Harden',
@@ -13,92 +13,15 @@ let data = {
   stars: ['kobe', 'jordan', 'wade']
 }
 
-// 收集依赖的篮子
-let basket
-
-function walk(obj) {
-  Object.keys(obj).forEach((key) => {
-    const deps = []
-    // 递归 以对data深度观测
-    let value = obj[key]
-    if (Object.prototype.toString.call(value) === '[object Object]') {
-      walk(value)
-    }
-
-    Object.defineProperty(obj, key, {
-      get () {
-        deps.push(basket)
-        return value
-      },
-      set (newVal) {
-        value = newVal
-        deps.forEach((fn) => {
-          fn()
-        })
-      }
-    })
-  })
-}
-
-walk(data)
-
-function myWatch(target, depFn) {
-  basket = depFn
-  if (typeof target === 'function') {
-    target()
-    return
-  }
-  if (/\./.test(target)) {
-    let helpData = data
-    target.split('.').forEach((path) => {
-      helpData = helpData[path]
-    })
-    return
-  }
-  data[target]
-}
-
 function render() {
   document.body.innerText = `The last season's MVP is ${data.name}, he's from ${data.team}, he has a teammate ${data.teammate.name}, ${data.teammate.name} was played for ${data.teammate.team.before}`
 }
 
-// 要代理的变异方法集合
-const methodsToPatch = [
-  'push',
-  'pop',
-  'shift',
-  'unshift',
-  'splice',
-  'sort',
-  'reverse'
-]
-const arrayProto = Array.prototype
-const arrayMethods = Object.create(arrayProto)
-methodsToPatch.forEach((method) => {
-  arrayMethods[method] = function (...args) {
-    console.log('this', this)
-    arrayProto[method].apply(this, args)
-    console.log(`我捕获了数组的${method}方法`)
-  }
-})
+observe(data)
 
-data.stars.__proto__ = arrayMethods
+myWatch(render, render)
 
-// myWatch(render, render)
-// myWatch('name', () => {
-//   console.log('name被修改了', data.name)
-// })
-// myWatch('name', () => {
-//   console.log('name的第二个依赖', data.name)
-// })
+data.name = 123
+data.stars.push(1)
 
-// myWatch('teammate.team.before', () => {
-//   console.log('以前的球队被修改了了了')
-// })
 
-// data.name = 'eleven'
-// data.team = 'xian'
-// data.teammate.name = 'jason'
-// data.teammate.team.before = 'lvdian'
-// data.stars.push('nash')
-// console.log(data.stars)
---
+++ src/core/observer.js
@@ -0,0 +1,115 @@
+import { isPlainObj } from '../shared/util'
+
+class Observer {
+  constructor (value) {
+    if (Array.isArray(value)) { // 数组
+      value.__proto__ = arrayMethods
+      this.observeArray(value)
+    } else { // 对象
+      this.walk(value)
+    }
+  }
+
+  walk (obj) { // 逐一观察对象中的属性
+    const keys = Object.keys(obj)
+    keys.forEach((key) => {
+      defineReactive(obj, key)
+    })
+  }
+
+  observeArray (arr) { // 深度观测数组
+    arr.forEach(item => {
+      observe(item)
+    })
+  }
+}
+
+// 收集依赖的篮子
+let basket
+
+// 将某一属性定义为响应式属性
+function defineReactive (obj, key) {
+  const deps = []
+
+  const property = Object.getOwnPropertyDescriptor(obj, key)
+  if (property && !property.configurable) { // 如果指定的属性不在该对象上 会返回undefined
+    return
+  }
+
+  const getter = property && property.get // 缓存原来的getter和setter
+  const setter = property && property.set
+
+  let value = obj[key]
+  observe(value) // 实现深度观测
+
+  Object.defineProperty(obj, key, {
+    get () {
+      if (getter) {
+        value = getter.call(obj)
+      }
+      deps.push(basket)
+      return value
+    },
+    set (newVal) {
+      if (getter) {
+        value = getter.call(obj)
+      }
+      if (newVal === value || (newVal !== newVal && value !== value)) { // 新旧值相等的话什么也不做
+        return
+      }
+      if (setter) {
+        setter.call(obj, newVal)
+      } else {
+        value = newVal
+      }
+      observe(newVal) // 新值可能是对象 继续观察新值
+      deps.forEach((fn) => {
+        fn()
+      })
+    }
+  })
+}
+
+// 观察某一对象 将它转为响应式的
+export function observe (target) {
+  if (Array.isArray(target) || isPlainObj(target)) {
+    return new Observer(target)
+  }
+}
+
+// 要代理的变异方法集合
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+]
+const arrayProto = Array.prototype
+const arrayMethods = Object.create(arrayProto)
+methodsToPatch.forEach((method) => {
+  arrayMethods[method] = function (...args) {
+    arrayProto[method].apply(this, args)
+    console.log(`我捕获了数组的${method}方法`)
+  }
+})
+
+export function myWatch (exp, depFn) {
+  basket = depFn
+  if (typeof exp === 'function') {
+    exp()
+    return
+  }
+  if (/\./.test(exp)) {
+    let helpData = data
+    exp.split('.').forEach((path) => {
+      helpData = helpData[path]
+    })
+    return
+  }
+  data[exp]
+}
+
+
